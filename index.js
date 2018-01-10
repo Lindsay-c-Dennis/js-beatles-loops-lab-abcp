6c83f343bf79d36f0edf8b90d3d626d0fcdfb5ab
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-10 15:39:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for (let i=0; i<4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`); 
+  }
+  return array;
+}
+function johnLennonFacts(facts) 
